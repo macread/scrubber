0efe46c4fb496ca797fba2f55dd43bbd47136c13
James 'Mac' Read <james.macgregor.read@gmail.com>
2019-12-18 15:43:35 -0700

    Started work on the event page

---
+++ src/components/Event/Event.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import ReactDOM from 'react-dom';
+
+import Event from './Event';
+
+it('renders without crashing', () => {
+    const div = document.createElement('div');
+
+    ReactDOM.render(
+        <Event
+            dialogProps={
+                {
+                    open: true,
+
+                    onClose: () => { }
+                }
+            }
+
+            content={<div></div>}
+        />,
+        div
+    );
+
+    ReactDOM.unmountComponentAtNode(div);
+});
